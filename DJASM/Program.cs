d8ffbbd6283f3254a5acbee144a41b8cb42ae64b
CotordoGames <buddendean@outlook.com>
2025-10-01 22:31:40 -0600

    Add files via upload

---
+++ DJASM/Program.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Diagnostics;
+using DJASM;
+
+
+namespace DJASM
+{
+    class Program
+    {
+        
+
+        public enum InstructionType
+        {
+            loadI, //loads immediate value. (register, value)
+            storeI, //stores immediate value. (address, value)
+            SwapR, //loads a registers value into another register. (register, register)
+            storeR, //stores the value of a register in memory. (register, address)
+            loadA, // loads the value of an adress into the selected register
+            jump, //jumps to the specified line
+            jumpZ, //jumps to the specified line if (addr) is 0
+            jumpL, //jumps id less than (addr)
+            jumpG, //jumps if greater than (addr)
+            jumpE, //jumps if equal to (addr)
+            Add, //adds 2 numbers (addr, addr, out)
+            Sub, //subtracts a from b (addr, addr)
+            delay, //delays instruction exectution by x ticks
+            NoOp
+        }
+
+        struct Instruction
+        {
+            public InstructionType type;
+            public List<string> arguments;
+
+            public Instruction(InstructionType Type, List<string> args)
+            {
+                type = Type;
+                arguments = args;
+            }
+        }
+        static List<Instruction> ParseDJFile(string file)
+        {
+            var instructions = new List<Instruction>();
+            var lines = File.ReadAllLines(file);
+
+            foreach(var rawline in lines)
+            {
+                string line = rawline.Split(';')[0].Trim();
+                if (string.IsNullOrWhiteSpace(line)) continue;
+
+                var tokens = line.Split(' ').ToList();
+
+                string command = tokens[0].ToLower();
+
+                InstructionType type = command switch
+                {
+                    "loadi" => InstructionType.loadI,
+                    "loadr" => InstructionType.SwapR,
+                    "storei" => InstructionType.storeI,
+                    "storer" => InstructionType.storeR,
+                    "jump" => InstructionType.jump,
+                    "jumpz" => InstructionType.jumpZ,
+                    "add" => InstructionType.Add,
+                    "loada" => InstructionType.loadA,
+                    "delay" => InstructionType.delay,
+                    _ => InstructionType.NoOp,
+                };
+                instructions.Add(new Instruction(type, tokens));
+            }
+            return instructions;
+        }
+
+        static void ExecuteInstructions(List<Instruction> instructions, byte[] RAM)
+        {
+            int printaddr = 16383;
+            byte r1 = 0;
+            byte r2 = 0;
+            int idx = 0;
+            while(idx < instructions.Count)
+            {
+                var instr = instructions[idx];
+                var parts = instr.arguments;
+
+                switch(instr.type)
+                {
+                    case InstructionType.loadI:
+                        if (parts[1].Contains("1"))
+                        {
+                            r1 = Convert.ToByte(parts[2]);
+
+                        }
+                        else if (parts[1].Contains("2"))
+                        {
+                            r2 = Convert.ToByte(parts[2]);
+                        }
+                        idx++;
+                        break;
+
+                    case InstructionType.SwapR:
+                        if (parts[1].Contains("1"))
+                        {
+                            r1 = r2;
+                        }
+                        else if (parts[1].Contains("2"))
+                        {
+                            r2 = r1;
+                        }
+                        idx++;
+                        break;
+
+                    case InstructionType.NoOp:
+                        idx++;
+                        break;
+
+                    case InstructionType.storeI:
+                        RAM[Convert.ToUInt16(parts[1])] = Convert.ToByte(parts[2]);
+                        if (RAM[0] != 0)
+                        {
+                            Console.Write((char)RAM[printaddr]);
+                        }
+                        idx++;
+                        break;
+
+                    case InstructionType.storeR:
+                        if (parts[1].Contains("1"))
+                        {
+                            RAM[Convert.ToUInt16(parts[2])] = r1;
+                        }
+                        else if (parts[1].Contains("2"))
+                        {
+                            RAM[Convert.ToUInt16(parts[2])] = r2;
+                        }
+                        if (RAM[printaddr] != 0)
+                        {
+                            Console.Write((char)RAM[printaddr]);
+                        }
+                        idx++;
+                        break;
+
+                    case InstructionType.jump:
+                        idx = Convert.ToUInt16(parts[1]);
+                        break;
+
+                    case InstructionType.jumpZ:
+                        if (Convert.ToUInt16(parts[1]) == 0)
+                        {
+                            idx = Convert.ToUInt16(parts[1]);
+                        }
+                        else
+                        {
+                            idx++;
+                        }
+                        break;
+
+                    case InstructionType.Add:
+                        RAM[Convert.ToInt16(parts[3])] = Convert.ToByte(Convert.ToByte(parts[1]) + Convert.ToByte(parts[2]));
+                        idx++;
+                        break;
+
+                    case InstructionType.loadA:
+                        if (parts[1].Contains("1"))
+                        {
+                            r1 = RAM[Convert.ToUInt16(parts[2])];
+                        }
+                        else if (parts[1].Contains("2"))
+                        {
+                           r2 = RAM[Convert.ToUInt16(parts[2])];
+                        }
+                        if (RAM[printaddr] != 0)
+                        {
+                            Console.Write((char)RAM[printaddr]);
+                        }
+                        idx++;
+                        break;
+
+                    case InstructionType.delay:
+                        Thread.Sleep(Convert.ToUInt16(parts[1]));
+                        idx++;
+                        break;
+                }
+                WindowCreation.UpdateScreen();
+
+
+            }
+        }
+
+        public static byte[] RAM = new byte[65536];
+        public static void Main()
+        {
+            
+
+            string FilePath = Path.Combine(Directory.GetCurrentDirectory(), "source.dj");
+
+            List<Instruction> Instructions = ParseDJFile(FilePath);
+
+            WindowCreation.CreateWindow();
+
+            ExecuteInstructions(Instructions, Program.RAM);
+        }
+    }
+}
---
+++ DJASM/Window.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using DJASM;
+using Raylib_cs;
+
+namespace DJASM
+{
+    class WindowCreation
+    {
+         static Color[] pallete = new Color[32];
+
+        static public void CreateWindow()
+        {
+            //gray or "white" colors
+            pallete[0] = new Color(0, 0, 0, 255);
+            pallete[1] = new Color(64, 64, 64, 255);
+            pallete[2] = new Color(96, 96, 96, 255);
+            pallete[3] = new Color(255, 255, 255, 255);
+            //red colors
+            pallete[4] = new Color(32, 0, 0, 255);
+            pallete[5] = new Color(64, 0, 0, 255);
+            pallete[6] = new Color(96, 0, 0, 255);
+            pallete[7] = new Color(255, 0, 0, 255);
+            //blue colors
+            pallete[8] = new Color(0, 32, 0, 255);
+            pallete[9] = new Color(0, 64, 0, 255);
+            pallete[10] = new Color(0, 96, 0, 255);
+            pallete[11] = new Color(0, 255, 0, 255);
+            //green colors
+            pallete[12] = new Color(0, 0, 32, 255);
+            pallete[13] = new Color(0, 0, 64, 255);
+            pallete[14] = new Color(0, 0, 96, 255);
+            pallete[15] = new Color(0, 0, 255, 255);
+            //yellow colors
+            pallete[16] = new Color(32, 0, 32, 255);
+            pallete[17] = new Color(64, 0, 64, 255);
+            pallete[18] = new Color(96, 0, 96, 255);
+            pallete[19] = new Color(255, 0, 255, 255);
+            //magenta colors
+            pallete[20] = new Color(32, 32, 0, 255);
+            pallete[21] = new Color(64, 64, 0, 255);
+            pallete[22] = new Color(96, 96, 0, 255);
+            pallete[23] = new Color(255, 255, 0, 255);
+            //teal colors
+            pallete[24] = new Color(0, 64, 64, 255);
+            pallete[25] = new Color(0, 64, 64, 255);
+            pallete[26] = new Color(0, 96, 96, 255);
+            pallete[27] = new Color(0, 255, 255, 255);
+            //placeholder
+            pallete[28] = new Color(0, 0, 0, 255);
+            pallete[29] = new Color(64, 64, 64, 255);
+            pallete[30] = new Color(96, 96, 96, 255);
+            pallete[31] = new Color(255, 255, 255, 255);
+
+
+            Raylib.InitWindow(1792, 1280, "DJASM PROGRAM");
+            Raylib.SetTargetFPS(1000);
+        }
+
+        static public void UpdateScreen()
+        {
+            
+            int winw = 224;
+            int winh = 160;
+            Raylib.BeginDrawing();
+
+
+            Raylib.BeginDrawing();
+            Raylib.ClearBackground(Color.Black);
+
+            for (int y = 0; y < winh; y++)
+            {
+                for (int x = 0; x < winw; x++)
+                {
+                    int addr = y * winw + x; // map (x,y) → RAM index
+                    byte colorIndex = Program.RAM[addr];
+                    Raylib.DrawRectangle(x * 8, y * 8, 8, 8, pallete[colorIndex]);
+                }
+            }
+
+            Raylib.EndDrawing();
+        }
+    }
+}
